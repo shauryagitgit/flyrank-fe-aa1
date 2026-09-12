251d88bdc622a8bc4bd6a3f94810fd5f1850b29f
shauryagitgit <shauryayay@gmail.com>
2026-09-12 23:08:25 +0530

    test: configure Playwright end-to-end testing

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  use: { baseURL: 'http://127.0.0.1:4173', trace: 'on-first-retry' },
+  webServer: { command: 'npm run dev -- --host 127.0.0.1', url: 'http://127.0.0.1:4173', reuseExistingServer: !process.env.CI },
+  reporter: [['html', { open: 'never' }], ['list']],
+});
